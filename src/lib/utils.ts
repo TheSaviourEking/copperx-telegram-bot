98998033d958980af78eff07479052c0d0b75190
TheSaviourEking <saviour.eking@cyberservices.com>
2025-03-20 03:21:11 +0100

    add utility function to generate introductory message for the bot

---
+++ src/lib/utils.ts
@@ -0,0 +1,10 @@
+export function getIntroMessage(): string {
+    return (
+        '👋 *Welcome to the Copperx Payout Bot!* \n\n' +
+        'This bot lets you manage your Copperx account directly in Telegram. ' +
+        'Deposit, withdraw, and transfer USDC with ease! To get started:\n' +
+        '- Use /login to authenticate with your Copperx account.\n' +
+        '- Check available commands with /help.\n\n' +
+        'Built with ❤️ by Saviour Eking (https://github.com/TheSaviourEking) | Support: https://t.me/copperxcommunity/2183'
+    );
+}
